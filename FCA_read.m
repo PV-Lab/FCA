ce579714515fdb958f992844dccb7d9f4cf7db6a
maljensen <jensenma@mit.edu>
2015-08-24 17:51:00 -0400

    Editing sample parameters for PA-3 data processing

--- FCA_read.m
+++ FCA_read.m
@@ -6,17 +6,17 @@ close all
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % define parameters
-bin=1; % binning resolution
-d=490*10^-4; % thickness of silicon wafer, cm
+bin=25; % binning resolution
+d=240*10^-4; % thickness of silicon wafer, cm
 % define the cut-off injection level. signal at low injection levels 
 % is very noisy
 cutoff_inject=1.0e13; %cm^-3
 %Define the cutoff time - this is similar to the above consideration
-cutoff_t=1e-3; %s
+cutoff_t=10e-3; %s
 T = 25+273.15; %sample temperature in K
 FCA = 7.5e-18; %FCA cross-section to be used, cm^2. 7.5e-18 is the value determined by Sin Cheng's work.
 inj_dep = 0; %Is the FCA cross-section injection dependent? This will also perform a correction for temperature if measurements are not made at room-temperature.
-save_file='20150821_FCAData_bin1_5303_25C.mat'; %filename for .mat file with raw data for fitting
+save_file='20150824_FCAData_bin25_3281_25C.mat'; %filename for .mat file with raw data for fitting
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Read the data and do some initial processing
