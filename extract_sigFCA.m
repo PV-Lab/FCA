1b5f7d06ac3cc48911aa702a9d0ca2a238877d80
maljensen <jensenma@mit.edu>
2015-08-28 16:57:29 -0400

    Correct script to extract FCA coefficient from filtered data

--- extract_sigFCA.m
+++ extract_sigFCA.m
@@ -1,18 +1,19 @@
-function extract_sigFCA
+function [G_Green,sigma,laserAvg_store,laserStd_store,power_transmitted,lhs_store]=extract_sigFCA
 
 clear all; 
 close all;
 
 d = 260e-4; %cm, sample thickness
+cutoff = 0.001; %filter design cutoff
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Read the laser power data
-laserpower = xlsread('C:\Users\Mallory\Documents\Lifetime spectroscopy\Experiments\FCA coeff August 27 2015','Laser Power'); 
+laserpower = xlsread('C:\Users\Mallory\Documents\Lifetime spectroscopy\Experiments\FCA coeff August 27 2015\Experiment log.xlsx','Laser Power'); 
 %column 1 = scan, column 4 = lambda, column 7 = avg, column 8 = std
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Read the log sample data
-sample_log = xlsread('C:\Users\Mallory\Documents\Lifetime spectroscopy\Experiments\FCA coeff August 27 2015','Log files+T'); 
+sample_log = xlsread('C:\Users\Mallory\Documents\Lifetime spectroscopy\Experiments\FCA coeff August 27 2015\Experiment log.xlsx','Log files+T'); 
 %column 2 = scan, column 4 = lambda, column 6 = filters
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -35,9 +36,8 @@ lambda = zeros(number,1);
 V0_store = zeros(number,1); 
 carrier_store = cell(number,1); 
 t_store = cell(number,1); 
-filters_store = cell(number,1); 
-
-figure; 
+filters_store = zeros(number,1); 
+diameter_store = zeros(number,1); 
 
 [m,n] = size(laserpower); 
 [p,q] = size(sample_log);
@@ -64,92 +64,111 @@ for i = 1:number
     laserAvg_store(i) = laserpower(index,7); 
     laserStd_store(i) = laserpower(index,8); 
     lambda(i) = laserpower(index,4); 
+    diameter_store(i) = laserpower(index,12);
     
     %Find the filter combination that match this filename
-    index = []; 
+    index = 0; 
     for j = 1:p
-        if isempty(strfind(filenames{i},sample_log(j,1)))==0
-            index = j;
-        else 
-            error('Problem matching filters');
+        if isempty(strfind(filenames{i},num2str(sample_log(j,2))))==0
+            if index ==0
+                index = j; 
+            else 
+                error('Problem matching laser power');
+            end
         end
     end 
     
-    filters_store{i} = sample_log(j,6); 
+    filters_store(i) = sample_log(index,6); 
     
 end
-
-% [T_sorted,IX] = sort(T_store); 
-% 
-% for i = 1:number
-%     
-%     product_plot(i) = product_store(IX(i)); 
-%     
-%     laserAvg_plot(i) = laserAvg_store(IX(i)); 
-%     laserStd_plot(i) = laserStd_store(IX(i)); 
-%     windowT_plot(i) = windowT_store(IX(i)); 
-%     lambda_plot(i) = lambda(IX(i)); 
-%     
-% end
-% 
-% 
-% figure;
-% h(1)=plot(T_sorted,product_plot,'.'); 
-% 
-% xlabel('Temperature (K)'); 
-% ylabel('\sigma*\Deltan at t = 0'); 
-% title('5 ns rise time + 5 ns averaging'); 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Process the data to extract sigma FCA
 
 %Take the first part of the curve calculate the FCA coefficient assuming
 %a constant generation rate for each measurement
+lhs_store = zeros(size(laserAvg_store)); %this vector will store the left hand side of the sigma_FCA equation
+G_Green = zeros(size(laserAvg_store));
+sG_Green = zeros(size(laserAvg_store)); 
+sigma = zeros(size(laserAvg_store));
+power_transmitted = zeros(size(laserAvg_store));
 
+for i = 1:length(laserAvg_store)
+    
+    carrier = carrier_store{i};
+    t = t_store{i}; 
+    
+    %Let's filter the data for a better result
+    [coeff1,coeff2] = butter(1,cutoff);
+    carrier_filt = filtfilt(coeff1,coeff2,carrier); 
+    
+    %Find the maximum value of the data and then take the average of 2
+    %points on either side
+    max_index = find(carrier_filt == max(carrier_filt)); 
+    max_values = carrier_filt(max_index-2:max_index+2); 
+    max_avg = mean(max_values);
+    
+    figure;
+    plot(t,carrier);
+    hold all;
+    plot(t,carrier_filt);
+    hold all;
+    plot(t(max_index),max_avg,'o','MarkerSize',8); 
+    xlabel('Time (s)'); 
+    ylabel('-log(1-V/V_0)'); 
+    
+    lhs_store(i) = max_avg/d; 
+    
+    %Get the right hand side of the equation - the carrier density
+    if filters_store(i)==1
+        T = NDfilters('NE05A',lambda(i)); 
+    elseif filters_store(i)==2
+        T = NDfilters('NE06A',lambda(i)); 
+    elseif filters_store(i)==3
+        T = NDfilters('NE10A',lambda(i)); 
+    elseif filters_store(i)==4
+        T = NDfilters('NE13A',lambda(i)); 
+    elseif filters_store(i)==5
+        T1 = NDfilters('NE05A',lambda(i)); 
+        T2 = NDfilters('NE06A',lambda(i));
+        T = T1*T2;
+    elseif filters_store(i)==6
+        T1 = NDfilters('NE06A',lambda(i)); 
+        T2 = NDfilters('NE10A',lambda(i));
+        T = T1*T2;
+    elseif filters_store(i)==7
+        T1 = NDfilters('NE06A',lambda(i)); 
+        T2 = NDfilters('NE13A',lambda(i));
+        T = T1*T2;
+    elseif filters_store(i)==0
+        T=1;
+    else
+        disp('Did not succeed at finding transmission');
+    end
+    
+    power_transmitted(i) = laserAvg_store(i)*T; %uJ
+    std_transmitted = laserStd_store(i)*T; %uJ
 
-
-
-
-
-
-
-
-
-power_transmitted = laserAvg_plot.*T_NE06A.*T_NE13A; % uJ
-% model = 'Sturm';
-G_Green = length(power_transmitted); 
-sigma = length(power_transmitted); 
-laserStd_transmitted = laserStd_plot.*T_NE06A.*T_NE13A; %uJ
-
-for i = 1:length(power_transmitted)
-
-    [G_Green_hold,sG_Green_hold]=calcG_multipleR(power_transmitted(i)*1e-6,lambda_plot(i),260,T_sorted(i),windowT_plot(i),10,0.75,laserStd_transmitted(i)*1e-6,0.05);
+    [G_Green_hold,sG_Green_hold]=calcG_multipleR(power_transmitted(i)*1e-6,lambda(i),d,25+273.15,25+273.15,10,diameter_store(i),std_transmitted*1e-6,0.05);
+    
     G_Green(i) = sum(G_Green_hold); 
     sG_Green(i) = sqrt(sum(sG_Green_hold.^2)); 
-%     G_Sturm_hold = calcG_multipleR(power_transmitted(i)*1e-6,lambda_plot(i),260,T_sorted(i),windowT_plot(i),'Sturm',10,0.75);
-%     G_Sturm(i) = sum(G_Sturm_hold); 
-%     G_none(i) = calcG_noModel(power_transmitted(i)*1e-6,lambda(i),260);
-    sigma(i) = product_plot(i)/G_Green(i); %cm2
     
+    sigma(i) = lhs_store(i)/G_Green(i); 
+
 end
 
 figure;
-plot(T_sorted,sigma,'o','MarkerSize',8);
-xlabel('Temperature [K]','FontSize',20); 
+plot(G_Green,sigma,'o','MarkerSize',8);
+xlabel('Carrier density [cm^-^3]','FontSize',20); 
 ylabel('\sigma at t = 0 [cm^2]','FontSize',20);
-title('FZ wafer with NE06A+NE13A filter','FontSize',20); 
+title('FZ wafer with no passivation','FontSize',20); 
 
 figure;
-semilogy(T_sorted,G_Green,'o'); 
-xlabel('Temperature [K]','FontSize',20);
-ylabel('Generation rate [cm^{-3}]','FontSize',20); 
-legend('Green 2008');
-
-figure;
-errorbar(T_sorted,laserAvg_plot,laserStd_plot,'x');
+errorbar(laserAvg_store,laserStd_store,'x');
 hold all;
-plot(T_sorted,power_transmitted,'s');
-xlabel('Temperature [K]','FontSize',20);
+plot(power_transmitted,'s');
+xlabel('Measurement','FontSize',20);
 ylabel('Laser Power [\muJ]','FontSize',20);
 
-save('NE06A_NE13A_exp_5ns_noTdep_diffAvg_changeG_revGCalc_noQuartz_10R_wError_072215.mat','T_sorted','sigma','G_Green','product_plot','sG_Green'); 
+% save('NE06A_NE13A_exp_5ns_noTdep_diffAvg_changeG_revGCalc_noQuartz_10R_wError_072215.mat','T_sorted','sigma','G_Green','product_plot','sG_Green'); 
